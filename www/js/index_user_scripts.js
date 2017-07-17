021a0ac8e9a6bcf1f2b29957468fc26d7484b4fe
Sam Suresh <sam@mu.my>
2017-07-18 07:40:12 +0800

    Add files via upload

---
+++ www/js/index_user_scripts.js
@@ -0,0 +1,95 @@
+/*jshint browser:true */
+/*global $ */(function()
+{
+ "use strict";
+ /*
+   hook up event handlers 
+ */
+ function register_event_handlers()
+ {
+    
+    
+     /* button  Back */
+    $(document).on("click", ".uib_w_9", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#mainpage"); 
+         return false;
+    });
+    
+        /* button  .uib_w_1 */
+    $(document).on("click", ".uib_w_1", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#info"); 
+         return false;
+    });
+    
+        /* button  .uib_w_2 */
+    $(document).on("click", ".uib_w_2", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#ourservices"); 
+         return false;
+    });
+    
+        /* button  .uib_w_3 */
+    $(document).on("click", ".uib_w_3", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#galeri"); 
+         return false;
+    });
+    
+        /* button  .uib_w_6 */
+    $(document).on("click", ".uib_w_6", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#mainpage"); 
+         return false;
+    });
+    
+        /* button  .uib_w_8 */
+    $(document).on("click", ".uib_w_8", function(evt)
+    {
+         /*global uib_sb */
+         /* Other possible functions are: 
+           uib_sb.open_sidebar($sb)
+           uib_sb.close_sidebar($sb)
+           uib_sb.toggle_sidebar($sb)
+            uib_sb.close_all_sidebars()
+          See js/sidebar.js for the full sidebar API */
+        
+         uib_sb.toggle_sidebar($(".uib_w_7"));  
+         return false;
+    });
+    
+        /* button  .uib_w_11 */
+    $(document).on("click", ".uib_w_11", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#mainpage"); 
+         return false;
+    });
+    
+        /* button  .uib_w_12 */
+    
+    
+        /* button  .uib_w_12 */
+    $(document).on("click", ".uib_w_12", function(evt)
+    {
+         /*global uib_sb */
+         /* Other possible functions are: 
+           uib_sb.open_sidebar($sb)
+           uib_sb.close_sidebar($sb)
+           uib_sb.toggle_sidebar($sb)
+            uib_sb.close_all_sidebars()
+          See js/sidebar.js for the full sidebar API */
+        
+         uib_sb.toggle_sidebar($(".uib_w_7"));  
+         return false;
+    });
+    
+    }
+ document.addEventListener("app.Ready", register_event_handlers, false);
+})();
